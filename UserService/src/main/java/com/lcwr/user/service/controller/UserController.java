f1f034a4afcd5c19e723ae20d9efa01d0c1d0012
raj bora <boraraj682@gmail.com>
2023-07-31 17:12:31 +0530

    implementing curcuitbreaker using resilence4J

--- UserService/src/main/java/com/lcwr/user/service/controller/UserController.java
+++ UserService/src/main/java/com/lcwr/user/service/controller/UserController.java
@@ -3,6 +3,10 @@ package com.lcwr.user.service.controller;
 
 import com.lcwr.user.service.entity.User;
 import com.lcwr.user.service.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +14,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-@Autowired
+  @Autowired
     private UserService userService;
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
 
 // create user
@@ -27,12 +34,27 @@ public class UserController {
 
      }
 
+     int retry_count=0;
      // get single user
 @GetMapping("/{userId}")
+// @CircuitBreaker(name ="ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+@Retry(name="ratingHotelService" , fallbackMethod ="ratingHotelFallback" )
 public  ResponseEntity<User> getSingleUser( @PathVariable  String userId){
 
-              User user=userService.getUser(userId);
+    logger.info ("retry method times :{}",retry_count);
+    retry_count ++;
+    User user=userService.getUser(userId);
                   return ResponseEntity.ok(user);
+}
+
+public  ResponseEntity<User> ratingHotelFallback(String userId, Exception ex)
+{
+
+ //  logger.info("Fallback is executed because service is down "+ex.getMessage());
+
+   User user= User.builder().email("Dummy@gmail.com").name("Dummy").userId("1234").about("this user is created because dummy file is needed").build();
+    return  new ResponseEntity<>(user, HttpStatus.OK);
+
 }
 
 
